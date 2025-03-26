2100696b11d51a2478080a4fb2b73c944723ae7c
1vet4 <113985634+1vet4@users.noreply.github.com>
2025-03-26 16:07:23 +0200

    Update big_data_1.py

--- big_data_1.py
+++ big_data_1.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     data_path = r'C:\Users\Iveta\PycharmProjects\parametric\aisdk-2025-03-11.csv'
     data = pd.read_csv(data_path, nrows=10000)
 
-    one, two, three = service.process_data_in_parallel()
-    #one, two, three = service.process_data_in_sequence()
+    distance_anomalies, position_anomalies, speed_anomalies = service.process_data_in_parallel()
+    #distance_anomalies, position_anomalies, speed_anomalies = service.process_data_in_sequence()
